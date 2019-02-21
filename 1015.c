1783cacc4b0c298f53874a9e0c68cd5c6856c25d
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2019-02-20 22:47:16 -0300

    Update 1015.c

--- 1015.c
+++ 1015.c
@@ -9,10 +9,10 @@ int main (void){
     scanf("%f%f", &x1, &y1);
     scanf("%f%f", &x2, &y2);
 	
-	  p1 = pow((x2-x1), 2);
+    p1 = pow((x2-x1), 2);
     p2 = pow((y2-y1), 2);
 	
-	  distancia = sqrt((p1+p2));
+    distancia = sqrt((p1+p2));
 
     printf("%.4lf\n", distancia);
 
